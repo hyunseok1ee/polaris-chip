f6e1d2b754569dbea0e6aa1df71f407b026673e8
Hyunseok Lee <hxl5600@psu.edu>
2026-02-15 21:03:10 -0500

    Added fancy, details, summary, and <slot>

--- src/my-card.js
+++ src/my-card.js
@@ -2,7 +2,7 @@ import { LitElement, html, css } from 'lit';
 
 /**
  * Now it's your turn. Here's what we need to try and do:
- * 1. Get you HTML from your card working in here 
+ * 1. Get you HTML from your card working in here
  * 2. Get your CSS rescoped as needed to work here
  */
 
@@ -17,14 +17,22 @@ export class MyCard extends LitElement {
     this.title = "Liverpool FC";
     this.image = "https://upload.wikimedia.org/wikipedia/en/0/0c/Liverpool_FC.svg";
     this.alt = "Liverpool FC crest";
+    this.desc = "A football club with a rich history and passionate supporters.";
+    this.fancy = false;
   }
 
   static get styles() {
     return css`
       :host {
-        display: block;
+        display: inline-block;
         font-size: 16px;
         font-family: Arial, sans-serif;
+      }
+
+      :host([fancy]) .card {
+        background-color: var(--my-card-fancy-bg, skyblue);
+        border: 2px solid black;
+        box-shadow: 10px 5px 5px blue;
       }
 
       .card {
@@ -32,7 +40,7 @@ export class MyCard extends LitElement {
         height: 460px;
         border: 1px solid #d9d9d9;
         border-radius: 20px;
-        background: white;
+        background-color: var(--my-card-background-color, white);
         padding: 18px;
         box-sizing: border-box;
         display: flex;
@@ -55,15 +63,31 @@ export class MyCard extends LitElement {
         margin: 16px 0 10px 0;
         font-size: 26px;
         text-align: center;
+        font-weight: bold;
       }
 
-      .card-desc {
-        margin: 0;
+      details summary {
+        text-align: left;
+        font-size: 18px;
+        padding: 8px 0;
+        width: 100%;
+      }
+
+      details[open] summary {
+        font-weight: bold;
+      }
+
+      .flowing {
+        border: 2px solid black;
+        text-align: left;
+        padding: 8px;
+        height: 70px;
+        overflow: auto;
+        width: 100%;
+        box-sizing: border-box;
         font-size: 14px;
         line-height: 1.4;
         color: #555;
-        text-align: center;
-        width: 100%;
       }
 
       @media (max-width: 500px) {
@@ -84,16 +108,28 @@ export class MyCard extends LitElement {
     `;
   }
 
+  openChanged(e) {
+    console.log(e);
+    if (e.target.getAttribute('open') !== null) {
+      this.fancy = true;
+    }
+    else {
+      this.fancy = false;
+    }
+  }
+
   render() {
     return html`
       <div class="card">
         <img class="card-img" src="${this.image}" alt="${this.alt}" />
-
         <h3 class="card-title">${this.title}</h3>
 
-        <p class="card-desc">
-          A football club with a rich history and passionate supporters.
-        </p>
+        <details ?open="${this.fancy}" @toggle="${this.openChanged}">
+          <summary>Description</summary>
+          <div class="flowing">
+            <slot>${this.desc}</slot>
+          </div>
+        </details>
       </div>
     `;
   }
@@ -103,6 +139,8 @@ export class MyCard extends LitElement {
       title: { type: String },
       image: { type: String },
       alt: { type: String },
+      desc: { type: String },
+      fancy: { type: Boolean, reflect: true },
     };
   }
 }
